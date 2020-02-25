394270bda2ac021299c08f9a8e8b4d3d2eaee31f
Simeon Kolev <simeon.kolev9@gmail.com>
2020-02-25 05:24:15 +0200

    Fixes.

---
+++ assets/environment/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="tileset" tilewidth="16" tileheight="16" tilecount="575" columns="25">
+ <image source="tileset.png" width="400" height="368"/>
+</tileset>
--- assets/js/game.js
+++ assets/js/game.js
@@ -18,29 +18,29 @@ var Boot = new Phaser.Class({
         this.load.image('brannie-btn', 'assets/menu/brannie-btn.png');
     },
     create: function () {
-        this.background = this.add.tileSprite(config.scale.width / 2, config.scale.height / 2, config.scale.width, config.scale.height, 'background');
+        this.background = this.add.tileSprite(config.width / 2, config.height / 2, config.width, config.height, 'background');
 
-        this.menu_background = this.add.image(config.scale.width / 2, config.scale.height / 2, 'menu-background');
-        this.play_btn = this.add.image(config.scale.width / 2, 70, 'play-btn').setInteractive();
-        this.options_btn = this.add.image(config.scale.width / 2, 100, 'options-btn').setInteractive();
-        this.exit_btn = this.add.image(config.scale.width / 2, 130, 'exit-btn').setInteractive();
+        this.menu_background = this.add.image(config.width / 2, config.height / 2, 'menu-background');
+        this.play_btn = this.add.image(config.width / 2, 70, 'play-btn').setInteractive();
+        this.options_btn = this.add.image(config.width / 2, 100, 'options-btn').setInteractive();
+        this.exit_btn = this.add.image(config.width / 2, 130, 'exit-btn').setInteractive();
 
-        this.player1 = this.add.image(config.scale.width / 2 - 40, 145, 'player1');
-        this.player2 = this.add.image(config.scale.width / 2 + 40, 145, 'player2');
+        this.player1 = this.add.image(config.width / 2 - 40, 145, 'player1');
+        this.player2 = this.add.image(config.width / 2 + 40, 145, 'player2');
 
         this.menu_overlay = this.add.graphics().setVisible(false);
-        this.menu_overlay.fillRect(0, 0, config.scale.width, config.scale.height, 0x000000);
+        this.menu_overlay.fillRect(0, 0, config.width, config.height, 0x000000);
         this.menu_overlay.fillStyle(0x000000, 0.3);
 
-        this.options_background = this.add.image(config.scale.width / 2, config.scale.height / 2, 'options-background').setVisible(false);
+        this.options_background = this.add.image(config.width / 2, config.height / 2, 'options-background').setVisible(false);
 
-        this.champion_select_background = this.add.image(config.scale.width / 2, config.scale.height / 2, 'champion-select-background').setVisible(false);
-        this.yannie_btn = this.add.image(config.scale.width / 2 + 20, config.scale.height / 2 - 10, 'yannie-btn').setVisible(false).setInteractive();
-        this.brannie_btn = this.add.image(config.scale.width / 2 + 20, config.scale.height / 2 + 25, 'brannie-btn').setVisible(false).setInteractive();
-        this.yannie_image = this.add.image(config.scale.width / 2 - 40, config.scale.height / 2 - 15, 'player2').setVisible(false);
-        this.brannie_image = this.add.image(config.scale.width / 2 - 35, config.scale.height / 2 + 15, 'player1').setVisible(false);
+        this.champion_select_background = this.add.image(config.width / 2, config.height / 2, 'champion-select-background').setVisible(false);
+        this.yannie_btn = this.add.image(config.width / 2 + 20, config.height / 2 - 10, 'yannie-btn').setVisible(false).setInteractive();
+        this.brannie_btn = this.add.image(config.width / 2 + 20, config.height / 2 + 25, 'brannie-btn').setVisible(false).setInteractive();
+        this.yannie_image = this.add.image(config.width / 2 - 40, config.height / 2 - 15, 'player2').setVisible(false);
+        this.brannie_image = this.add.image(config.width / 2 - 35, config.height / 2 + 15, 'player1').setVisible(false);
 
-        this.close_btn = this.add.image(config.scale.width / 2, 152, 'close-btn').setVisible(false).setInteractive();
+        this.close_btn = this.add.image(config.width / 2, 152, 'close-btn').setVisible(false).setInteractive();
 
         this.play_btn.on('pointerdown', this.openChampionSelectMenu, this);
         this.options_btn.on('pointerdown', this.openOptionsMenu, this);
@@ -104,20 +104,147 @@ var Boot = new Phaser.Class({
         }
     },
     play: function (character) {
-        alert('play with ' + character);
+        // alert('play with ' + character);
+        this.scene.start('game');
     }
+});
+
+var Game = new Phaser.Class({
+    Extends: Phaser.Scene,
+    player: null,
+    cursors: null,
+    groundLayer: null,
+    initialize: function Game() {
+        Phaser.Scene.call(this, {key: 'game', active: true});
+    },
+    preload: function () {
+        this.load.tilemapTiledJSON('map', 'assets/environment/map.json');
+        this.load.spritesheet('props', 'assets/environment/props.png', {frameWidth: 16, frameHeight: 16});
+        this.load.spritesheet('tileset', 'assets/environment/tileset.png', {frameWidth: 16, frameHeight: 16});
+        this.load.atlas('player', 'assets/player/player.png', 'assets/player/player.json');
+    },
+    create: function () {
+        var map = this.make.tilemap({key: 'map'});
+        var props = map.addTilesetImage('props');
+        var tileset = map.addTilesetImage('tileset');
+
+        this.groundLayer = map.createDynamicLayer('World', [tileset, props], 0, 0);
+        this.groundLayer.setCollisionByExclusion([-1]);
+
+        this.physics.world.bounds.width = this.groundLayer.width;
+        this.physics.world.bounds.height = this.groundLayer.height;
+
+        this.player = this.physics.add.sprite(150, 175, 'player');
+        this.player.setCollideWorldBounds(true);
+        this.player.body.setSize(14, 19);
+        this.player.body.setOffset(9, 12);
+
+        this.physics.add.collider(this.groundLayer, this.player);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+        this.cameras.main.startFollow(this.player);
+        this.cameras.main.setBackgroundColor('#ccccff');
+
+        this.anims.create({
+            key: 'idle',
+            frames: this.anims.generateFrameNames('player', {prefix: 'player-idle-', start: 1, end: 4, zeroPad: 1}),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'run',
+            frames: this.anims.generateFrameNames('player', {prefix: 'player-run-', start: 1, end: 6, zeroPad: 1}),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'jump',
+            frames: this.anims.generateFrameNames('player', {prefix: 'player-jump-', start: 1, end: 2, zeroPad: 1}),
+            frameRate: 2,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'crouch',
+            frames: this.anims.generateFrameNames('player', {prefix: 'player-crouch-', start: 1, end: 2, zeroPad: 1}),
+            frameRate: 6,
+            repeat: -1
+        });
+    },
+    update: function (time, delta) {
+        var movementSpeed = 100;
+        var isOnFloor = this.player.body.onFloor();
+        var shouldJump = this.cursors.space.isDown || this.cursors.up.isDown;
+        var shouldCrouch = this.cursors.down.isDown;
+
+        if (shouldCrouch && isOnFloor) {
+            movementSpeed /= 1.8;
+            this.player.body.setSize(15, 16);
+            this.player.body.setOffset(9, 15);
+        } else {
+            this.player.body.setSize(15, 19);
+            this.player.body.setOffset(9, 12);
+        }
+
+        if (this.cursors.left.isDown) {
+            if (isOnFloor && !shouldCrouch) {
+                this.player.anims.play('run', true);
+            }
+
+            this.player.body.setVelocityX(-movementSpeed);
+            this.player.flipX = true;
+        } else if (this.cursors.right.isDown) {
+            if (isOnFloor && !shouldCrouch) {
+                this.player.anims.play('run', true);
+            }
+
+            this.player.body.setVelocityX(movementSpeed);
+            this.player.flipX = false;
+        } else if (isOnFloor) {
+            if (!shouldCrouch) {
+                this.player.anims.play('idle', true);
+            }
+
+            this.player.setVelocityX(0);
+        }
+
+        if (shouldJump && isOnFloor) {
+            this.player.anims.play('jump', true);
+            this.player.body.setVelocityY(-500);
+        } else if (shouldCrouch && isOnFloor) {
+            this.player.anims.play('crouch', true);
+
+            if (!this.cursors.left.isDown && !this.cursors.right.isDown) {
+                this.player.setVelocityX(0);
+            }
+        }
+    },
 });
 
 var config = {
     type: Phaser.AUTO,
     backgroundColor: '#000000',
-    scene: [Boot],
+    scene: [/*Boot, */Game],
+    parent: 'game',
     scale: {
         mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
         parent: 'game',
         width: 288,
         height: 192
     },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y: 1000},
+            debug: true
+        }
+    },
+    pixelArt: true
 };
 
 var game = new Phaser.Game(config);
