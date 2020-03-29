2ddf3487c44e6826824dc6af66d60c1a14d86e82
Simeon Kolev <simeon.kolev9@gmail.com>
2020-03-30 00:34:46 +0300

    Fixes.

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -204,7 +204,7 @@ var Game = new Phaser.Class({
         this.gameOverSound = this.sound.add('game_over');
         this.gameSuccessSound = this.sound.add('game_success');
 
-        this.socket = io('http://localhost:8081/', { query: "playerName=" + encodeURIComponent(playerName) + "&characterType=" + encodeURIComponent(characterType) });
+        this.socket = io('http://simeonkolev.com:8081/', { query: "playerName=" + encodeURIComponent(playerName) + "&characterType=" + encodeURIComponent(characterType) });
         this.otherPlayers = this.physics.add.group({
             allowGravity: false
         });
